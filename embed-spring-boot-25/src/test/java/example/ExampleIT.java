415b37aa7f3807d6e748e52a71e2da76117b331d
Michael Mosmann <michael@mosmann.de>
2023-08-27 11:35:29 +0200

    canary works for spring 2.5.x

---
+++ embed-spring-boot-25/src/test/java/example/ExampleIT.java
@@ -0,0 +1,27 @@
+package example;
+
+import com.mongodb.assertions.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+@ExtendWith(SpringExtension.class)
+class ExampleIT {
+    @Test
+    void example(@Autowired final MongoTemplate mongoTemplate) {
+        Assertions.assertNotNull(mongoTemplate.getDb());
+        ArrayList<String> collectionNames = mongoTemplate.getDb()
+          .listCollectionNames()
+          .into(new ArrayList<>());
+        assertThat(collectionNames).isEmpty();
+    }
+}
