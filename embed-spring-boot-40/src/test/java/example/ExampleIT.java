2bdb9ab39afe709fa7541f04c5d9c5e714d90339
Ninad Tungare <ninad.tungare@gmail.com>
2026-01-03 15:53:13 +0530

    test updated spring imports

--- embed-spring-boot-40/src/test/java/example/ExampleIT.java
+++ embed-spring-boot-40/src/test/java/example/ExampleIT.java
@@ -1,31 +1,30 @@
 package example;
 
 import com.mongodb.assertions.Assertions;
+import de.flapdoodle.embed.mongo.spring.autoconfigure.MongodWrapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.data.mongodb.test.autoconfigure.AutoConfigureDataMongo;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@AutoConfigureDataMongo
-@SpringBootTest()
-@EnableAutoConfiguration
-@DirtiesContext
+@DataMongoTest()
+@ExtendWith(SpringExtension.class)
 class ExampleIT {
     @Test
-    void example(@Autowired final MongoTemplate mongoTemplate) {
+    void example(@Autowired final MongoTemplate mongoTemplate, @Autowired final ApplicationContext applicationContext) {
         Assertions.assertNotNull(mongoTemplate.getDb());
         ArrayList<String> collectionNames = mongoTemplate.getDb()
           .listCollectionNames()
           .into(new ArrayList<>());
         assertThat(collectionNames).isEmpty();
+        assertThat(applicationContext.getBeanNamesForType(MongodWrapper.class)).isNotEmpty();
+        assertThat(applicationContext.getBean(MongodWrapper.class)).isNotNull();
     }
 }
